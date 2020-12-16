b1da5301e83554c302f15d5ce4330ce0e5a9a955
Chaitanya4000 <60195247+Chaitanya4000@users.noreply.github.com>
2020-12-16 22:49:39 +0530

    Add files via upload
    Version1

---
+++ app.py
@@ -0,0 +1,26 @@
+import pickle
+import os
+from googletrans import Translator, constants
+from pprint import pprint
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+try:
+    translator = Translator()
+    print('Translator initialized successfully')
+except Exception as e:
+    print(str(e))
+
+
+@app.route('/LanguageTranslatorEngine', methods=['POST'])
+def languagetranslatorengine():
+    str_input = request.json['str_inputvalue']
+    dest_Language = request.json['str_destLang']
+    TranslatedText = translator.translate(str_input,dest=dest_Language)
+    return jsonify({'results': TranslatedText})
+
+
+if __name__ == "__main__":
+    app.run()
